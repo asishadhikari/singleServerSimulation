fa6bba055bd8ad4c5caaf7665894fb80d5d1dcfb
= <=>
2017-10-05 17:35:40 -0400

    readme testing

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "simlib/src/simlib.h"
+
+#define EVENT_ARRIVAL 1    //Event type for arrival event
+#define EVENT_DEPARTURE 2  //Event type for departure
+#define LIST_QUEUE 1       //
+#define LIST_SERVER 2
+
+int main(){
+	return 0;
+}
---
+++ src/readme.md
@@ -0,0 +1,2 @@
+#Single Server Simulation using simlib
+**This is a project on creating a single server based queuing system
