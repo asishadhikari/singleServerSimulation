20868e99ec666fce3c577b62e7892c7d8208b1a2
= <=>
2017-10-06 01:30:34 -0400

    need to fix ordering in main and the header files

--- main.cpp
+++ main.cpp
@@ -3,18 +3,7 @@
 #include <math.h>
 ///#include "lcgrand.h"  //Implementation rematining ###########################################
 #include "simFunctions.h"
-#define Q_LIMIT 100  //maximum customers in allowed in queue
-#define BUSY 1						//server busy
-#define IDLE 0						//server idle
 
-
-int num_custs_delayed, num_delays_required,
-    next_event_type, num_events, num_in_queue,
-    server_status; //state variables
-
-float avg_num_in_q, avg_server_util, avg_interarrival,
-    avg_service_time, sim_time, time_arrival[Q_LIMIT+1], 
-    time_last_event, time_next_event[3],total_delays;
 
 FILE *in, *out;
 // declare non simlib functions:
@@ -28,7 +17,36 @@ int main(){
 	num_events = 2;
 	//load the user defined interarrival, simulation number, service time into global vars
 	fscanf(in,"%f %f %d", &avg_interarrival, &avg_service_time, &num_delays_required);
-	initialize();
+	fprintf(out, "\n\n\n\n\n\n This is a SINGLE SERVER QUEUE SIMULATION LOG:: \n\n");
+	fprintf(out,"Given Average Interarrival Time is: %f\n",avg_interarrival);
+	fprintf(out,"Given Average Service Time is: %f\n",avg_service_time);
+	fprintf(out,"Required number of simulation runs: %d\n",num_delays_required);
+	
+	
+	initialize(); //reset all state vars, statistical counters, and clock variables (incl time of last event)
+
+	//keep running until the given number of delays not met
+	while (num_custs_delayed < num_delays_required){
+	  //updates 
+	  timing();
+	  
+	  update_stats();
+	  switch(next_event_type){
+	  	case 1:
+	  		arrive();
+	  		break;
+
+	  	case 2:
+	  	  depart();
+	  	  break;
+	  }
+	}
+	//log to output.txt
+	report();
+
+	fclose(in);
+	fclose(out);
+
 	
 	return 0;
 }
--- simFunctions.cpp
+++ simFunctions.cpp
@@ -1,3 +1,16 @@
+#include "simFunctions.h"
+#define Q_LIMIT 200  //maximum customers in allowed in queue
+#define BUSY 1						//server busy
+#define IDLE 0						//server idle
+
+
+int num_custs_delayed, num_delays_required,
+    next_event_type, num_events, num_in_queue,
+    server_status; //state variables
+
+float avg_num_in_q, avg_server_util, avg_interarrival,
+    avg_service_time, sim_time, time_arrival[Q_LIMIT+1], 
+    time_last_event, time_next_event[3],total_delays;
 void initialize(){
 	num_custs_delayed = 0;
 	next_event_type = 1;
@@ -8,7 +21,7 @@ void initialize(){
 	sim_time = 0.0;
 	time_last_event = 0.0;
 	time_next_event[0] = sim_time + generator();
-	time_next_event[1] = 100000000000000000000;
+	time_next_event[1] = 1.0e30;
 
 
 	avg_num_in_q = 0.0;
@@ -17,9 +30,9 @@ void initialize(){
 	total_delays = 0.0;
 
 }
-void timing();
-void arrive();
-void depart();
-void report();
-void update_stats();
-float generator();
+void timing(){}
+void arrive(){}
+void depart(){}
+void report(){}
+void update_stats(){}
+float generator(){}
